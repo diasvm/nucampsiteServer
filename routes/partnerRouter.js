d7d622f41199f0e03f6d7db0e5e2293a98c98dac
Victor Dias <diasvm@hotmail.com>
2024-01-19 00:30:34 -0800

    2. Exercise: Cross-Origin Resource Sharing

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -1,39 +1,46 @@
 const express = require("express");
-const Promotion = require("../models/promotion");
+const Partner = require("../models/partner");
 const authenticate = require("../authenticate");
+const cors = require('./cors');
+const partnerRouter = express.Router();
 
-const promotionRouter = express.Router();
-
-promotionRouter
+partnerRouter
   .route("/")
-  .get((req, res, next) => {
-    Promotion.find()
-      .then((promotions) => {
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+  .get(cors.cors, (req, res, next) => {
+    Partner.find()
+      .then((partners) => {
         res.statusCode = 200;
         res.setHeader("Content-Type", "application/json");
-        res.json(promotions);
+        res.json(partners);
       })
       .catch((err) => next(err));
   })
-  .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    Promotion.create(req.body)
-      .then((promotion) => {
-        console.log("Promotion Created ", promotion);
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
-        res.json(promotion);
-      })
-      .catch((err) => next(err));
-  })
-  .put((req, res) => {
-    res.statusCode = 403;
-    res.end("PUT operation not supported on /promotions");
-  })
-  .delete(
+  .post(
+    cors.corsWithOptions,
     authenticate.verifyUser,
     authenticate.verifyAdmin,
     (req, res, next) => {
-      Promotion.deleteMany()
+      Partner.create(req.body)
+        .then((partner) => {
+          console.log("Partner Created ", partner);
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(partner);
+        })
+        .catch((err) => next(err));
+    }
+  )
+  .put(cors.corsWithOptions, authenticate.verifyAdmin, (req, res) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /partners");
+  })
+  .delete(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      Partner.deleteMany()
         .then((response) => {
           res.statusCode = 200;
           res.setHeader("Content-Type", "application/json");
@@ -43,43 +50,55 @@ promotionRouter
     }
   );
 
-promotionRouter
-  .route("/:promotionId")
-  .get((req, res, next) => {
-    Promotion.findById(req.params.promotionId)
-      .then((promotion) => {
+partnerRouter
+  .route("/:partnerId")
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+  .get(cors.cors, (req, res, next) => {
+    Partner.findById(req.params.partnerId)
+      .then((partner) => {
         res.statusCode = 200;
         res.setHeader("Content-Type", "application/json");
-        res.json(promotion);
+        res.json(partner);
       })
       .catch((err) => next(err));
   })
-  .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
-    res.statusCode = 403;
-    res.end(
-      `POST operation not supported on /promotions/${req.params.promotionId}`
-    );
-  })
-  .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    Promotion.findByIdAndUpdate(
-      req.params.promotionId,
-      {
-        $set: req.body,
-      },
-      { new: true }
-    )
-      .then((promotion) => {
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
-        res.json(promotion);
-      })
-      .catch((err) => next(err));
-  })
-  .delete(
+  .post(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res) => {
+      res.statusCode = 403;
+      res.end(
+        `POST operation not supported on /partners/${req.params.partnerId}`
+      );
+    }
+  )
+  .put(
+    cors.corsWithOptions,
     authenticate.verifyUser,
     authenticate.verifyAdmin,
     (req, res, next) => {
-      Promotion.findByIdAndDelete(req.params.promotionId)
+      Partner.findByIdAndUpdate(
+        req.params.partnerId,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      )
+        .then((partner) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(partner);
+        })
+        .catch((err) => next(err));
+    }
+  )
+  .delete(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      Partner.findByIdAndDelete(req.params.partnerId)
         .then((response) => {
           res.statusCode = 200;
           res.setHeader("Content-Type", "application/json");
@@ -89,4 +108,4 @@ promotionRouter
     }
   );
 
-module.exports = promotionRouter;
+module.exports = partnerRouter;
